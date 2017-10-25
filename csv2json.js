204b6010e852badc164d8f95b2d5be43f1e0fdb3
audun hauge <audun.hauge@gmail.com>
2017-10-25 15:07:44 +0200

    kan nå konvertere mappe med xlsx til en elever.json

--- csv2json.js
+++ csv2json.js
@@ -1,25 +1,44 @@
-// leser inn en csv fil med elever og lager en json  fil som kan lastes inn i firebase
+// leser inn en xls fil med elever og lager en json  fil som kan lastes inn i firebase
+// BRUK:  node xls2json.js mappeNavn
 
 
-fs = require('fs');
-let file = process.argv[2];
-fs.readFile(file, 'utf8', function (err, data) {
-    if (err) throw err;
-    let lines = data.split('\n');
-    let i = 1;  // hopp over første linje
-    let l = lines.length - 1;
-    let stud = {};
-    while (i < l) {
-        let line = lines[i].toLocaleLowerCase().replace(/, /g,',');
-        let [enr,klasse,navn,kontakt,knavn] = line.split(';');
-        let [fn,ln] = navn.split(',');
-        stud[enr] = {klasse,fn,ln,kontakt};
-        i++;
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+const stud = {};
+
+let dir = process.argv[2];
+fs.readdirSync(dir).forEach(file => xl2j(dir, file));
+
+console.log(JSON.stringify(stud));
+
+
+function xl2j(dir, file) {
+    if (file.match('xls')) {
+        const result = excelToJson({
+            sourceFile: dir + '/' + file,
+            header: { rows: 1 },
+            columnToKey: {
+                A: 'enr',
+                B: 'klasse',
+                C: 'navn',
+                D: 'kontakt',
+            }
+
+        });
+
+
+        let entries = result[Object.keys(result)[0]];
+
+        for (s of entries) {
+            let { enr, klasse, navn, kontakt } = s;
+            let [ln, fn] = navn.toLocaleLowerCase().split(',');
+            if (klasse === undefined|| kontakt === undefined) {
+                console.error("mangelful",s);
+            }
+            kontakt = kontakt ? kontakt.toLocaleLowerCase() : '';
+            klasse = klasse ? klasse.toLocaleLowerCase() : '';
+            stud[enr] = { fn, ln, klasse, kontakt };
+        }
     }
-    console.log(JSON.stringify(stud));
-
-
-
-
-});
+}
 
